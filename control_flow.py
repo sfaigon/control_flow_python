3bf491e52e4b49b1a3d787f935eb8f264b94ca5e
chinobin <sfaigon@email.com>
2023-11-07 16:31:32 -0500

    uncomment code

--- control_flow.py
+++ control_flow.py
@@ -1,25 +1,25 @@
 # exercise-01 Vowel or Consonant
 
 
-# vowel = "aeiou"
-# letter = input("Please enter a letter from the alphabet (a-z or A-Z): ").lower()
-# if letter in vowel:
-#   print(f"{letter} is a vowel")
-# elif letter == "y":
-#     print(f"{letter} is sometimes a vowel")
-# else:
-#   print(f"{letter} is a consonant")
+vowel = "aeiou"
+letter = input("Please enter a letter from the alphabet (a-z or A-Z): ").lower()
+if letter in vowel:
+  print(f"{letter} is a vowel")
+elif letter == "y":
+    print(f"{letter} is sometimes a vowel")
+else:
+  print(f"{letter} is a consonant")
 
 
 
 
 # exercise-02 Length of Phrase
 
-# word = ""
-# while word != "quit":
-#     word = input("Please enter a word or phrase: ")
-#     if word != "quit":
-#         print(f"What you entered is {len(word)} characters long")
+word = ""
+while word != "quit":
+    word = input("Please enter a word or phrase: ")
+    if word != "quit":
+        print(f"What you entered is {len(word)} characters long")
 
 
 
@@ -28,14 +28,14 @@
 
 
 
-# age = int(input("Input a dog's age: "))
-# sum = 0
-# for i in range(age):
-#     if i<2:
-#         sum += 10
-#     else:
-#         sum += 7
-# print(f"The dog's age in dog years is {sum}")
+age = int(input("Input a dog's age: "))
+sum = 0
+for i in range(age):
+    if i<2:
+        sum += 10
+    else:
+        sum += 7
+print(f"The dog's age in dog years is {sum}")
 
 
 
@@ -45,18 +45,18 @@
 # exercise-04 What kind of Triangle?
 
 
-# print(" Enter the lengths of three side of a triangle:")
-# a = int(input("a:"))
-# b = int(input("b:"))
-# c = int(input("c:"))
+print(" Enter the lengths of three side of a triangle:")
+a = int(input("a:"))
+b = int(input("b:"))
+c = int(input("c:"))
 
-# if a == b and b == c:
-#     triangle = "equilateral"
-# elif a == b or b == c or a == c:
-#     triangle = "isosceles"
-# else:
-#     triangle = "scalene"
-# print(f"A triangle with side {a}, {b}, and {c} is a {triangle} triangle")
+if a == b and b == c:
+    triangle = "equilateral"
+elif a == b or b == c or a == c:
+    triangle = "isosceles"
+else:
+    triangle = "scalene"
+print(f"A triangle with side {a}, {b}, and {c} is a {triangle} triangle")
 
 
 
@@ -64,15 +64,15 @@
 
 # exercise-05 Fibonacci sequence for first 50 terms
 
-# num = 0
-# sum = 1
-# print("Term: 0 / number: 0")
-# print("Term: 1 / number: 1")
-# for i in range(2,50):
-#     holder = sum
-#     sum = sum + num
-#     num = holder
-#     print(f"Term: {i} / number: {sum}")
+num = 0
+sum = 1
+print("Term: 0 / number: 0")
+print("Term: 1 / number: 1")
+for i in range(2,50):
+    holder = sum
+    sum = sum + num
+    num = holder
+    print(f"Term: {i} / number: {sum}")
     
 
 
@@ -112,4 +112,8 @@ elif mm == "sep":
         season = "Fall"
     else:
         season = "Summer"
-print(f"{mm} {dd} is in {season}")
+
+if season != None:
+    print(f"{mm} {dd} is in {season}")
+else:
+    print("Date format entered in incorrect")
